a3796d6d0fc221e3c545a0673f185f9203d15906
helloWorldCindy <yusindy@rocketmail.com>
2017-09-25 21:19:49 -0400

    add weekly log

--- App.js
+++ App.js
@@ -1,64 +1,6 @@
 import {StackNavigator, TabNavigator} from 'react-navigation';
 import LoginScreen from './components/LoginScreen/LoginScreen';
 import SignupScreen from './components/SignupScreen/SignupScreen';
-<<<<<<< HEAD
-import ProtectedScreen from './components/ProtectedScreen/ProtectedScreen';
-import {firebaseApp} from './firebaseConfig';
-import Map from './components/Map/Map'
-
-class HomeScreen extends Component {
-  static navigationOptions = {
-    title: 'Retrieve',
-  };
-
-  state = {
-    isLogined: false,
-    loading: true
-  }
-
-  componentDidMount() {
-    this.removeAuthListener = firebaseApp.auth().onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({
-          isLogined: true,
-          loading: false
-        })
-      } else {
-        this.setState({
-          isLogined: false,
-          loading: false
-        })
-      }
-    });
-||||||| merged common ancestors
-import ProtectedScreen from './components/ProtectedScreen/ProtectedScreen';
-import {firebaseApp} from './firebaseConfig';
-
-class HomeScreen extends Component {
-  static navigationOptions = {
-    title: 'Retrieve',
-  };
-
-  state = {
-    isLogined: false,
-    loading: true
-  }
-
-  componentDidMount() {
-    this.removeAuthListener = firebaseApp.auth().onAuthStateChanged((user) => {
-      if (user) {
-        this.setState({
-          isLogined: true,
-          loading: false
-        })
-      } else {
-        this.setState({
-          isLogined: false,
-          loading: false
-        })
-      }
-    });
-=======
 import ProfileScreen from './components/ProfileScreen/ProfileScreen';
 import FoundPostsScreen from './components/FoundPostsSceen/FoundPostsScreen';
 import LostPostsScreen from './components/LostPostsScreen/LostPostsScreen';
@@ -73,7 +15,6 @@ const Tabs = TabNavigator({
   },
   LostPosts: {
     screen: LostPostsScreen
->>>>>>> 68314f039d07524c3635ddee8dc81a63836227ee
   }
 }, {
   initialRouteName: 'FoundPosts',
@@ -81,52 +22,7 @@ const Tabs = TabNavigator({
   tabBarOptions: {
     activeTintColor: '#e91e63'
   }
-<<<<<<< HEAD
-
-  render() {
-    const {navigation} = this.props;
-    const {isLogined, loading} = this.state;
-    const {navigate} = navigation;
-    if (loading) {
-      return <Text>Loading</Text>
-    }
-
-    if (isLogined) {
-      return (
-        <ProtectedScreen />
-      )
-    } else {
-      return (
-        <Map />
-      );
-    }
-  }
-}
-||||||| merged common ancestors
-
-  render() {
-    const {navigation} = this.props;
-    const {isLogined, loading} = this.state;
-    const {navigate} = navigation;
-
-    if (loading) {
-      return <Text>Loading</Text>
-    }
-
-    if (isLogined) {
-      return (
-        <ProtectedScreen />
-      )
-    } else {
-      return (
-        <LoginScreen navigate={navigate} />
-      );
-    }
-  }
-}
-=======
 });
->>>>>>> 68314f039d07524c3635ddee8dc81a63836227ee
 
 export default StackNavigator({
   Login: {screen: LoginScreen},
