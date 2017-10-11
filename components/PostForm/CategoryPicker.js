4f0a8d2758faa3494542a15d3e1ea4d24ddbb1a4
Jasonjys <jasonjys0511@gmail.com>
2017-10-11 17:58:03 -0400

    latest

--- components/PostForm/CategoryPicker.js
+++ components/PostForm/CategoryPicker.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {View} from 'react-native'
-import { Picker, List as AntList } from 'antd-mobile';
+import {Picker, List as AntList} from 'antd-mobile';
 
 class CategoryPicker extends Component {
   state = {
@@ -11,32 +11,25 @@ class CategoryPicker extends Component {
       {
         label: 'Eletronic',
         value: 'eletronic'
-      },
-      {
+      }, {
         label: 'Clothing/Shoes',
         value: 'clothingShoes'
-      },
-      {
+      }, {
         label: 'School/Office Supply',
         value: 'supply'
-      },
-      {
+      }, {
         label: 'Jewelry & Watch',
         value: 'jewelry'
-      },
-      {
+      }, {
         label: 'Pet',
         value: 'pet'
-      },
-      {
+      }, {
         label: 'Wallet/Card/Key',
         value: 'wck'
-      },
-      {
+      }, {
         label: 'Bag',
         value: 'bag'
-      },
-      {
+      }, {
         label: 'Other',
         value: 'other'
       }
@@ -45,17 +38,21 @@ class CategoryPicker extends Component {
   }
   render() {
     return (
-        <Picker
-          data={this.generateData()}
-          cols={1}
-          onChange={v => this.props.handleOnChange(v)}
-          value={this.props.categoryValue}
-          extra='Tap to choose'
-          okText='Confirm'
-          dismissText='Cancel'
-          style={{backgroundColor: 'white'}}>
-          <AntList.Item arrow="horizontal">Choose one category</AntList.Item>
-        </Picker>
+      <Picker
+        data={this.generateData()}
+        cols={1}
+        onChange={v => this
+        .props
+        .handleOnChange(v)}
+        value={this.props.categoryValue}
+        extra='Tap to choose'
+        okText='Confirm'
+        dismissText='Cancel'
+        style={{
+        backgroundColor: 'white'
+      }}>
+        <AntList.Item arrow="horizontal">Category</AntList.Item>
+      </Picker>
     )
   }
 }
