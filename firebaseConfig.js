88fdf01ef78fbf9ba8b579628649c1c081172e00
Shane Loong <Matthias@Shanes-MacBook-Pro.local>
2017-10-16 00:15:24 -0400

    Jason firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,22 +1,22 @@
 import * as firebase from 'firebase';
 
-// const config = {
-//     apiKey: "AIzaSyCy06AYOySGIMILWjBfCYBpG-HIWNqcHQw",
-//     authDomain: "retrieve-55769.firebaseapp.com",
-//     databaseURL: "https://retrieve-55769.firebaseio.com",
-//     projectId: "retrieve-55769",
-//     storageBucket: "",
-//     messagingSenderId: "569280491909"
-//   };
-
 const config = {
-apiKey: "AIzaSyBrxoHAQNjn08MWXXMwyivst0nC2N2zjUg",
-authDomain: "retrieve-a82e2.firebaseapp.com",
-databaseURL: "https://retrieve-a82e2.firebaseio.com",
-projectId: "retrieve-a82e2",
-storageBucket: "retrieve-a82e2.appspot.com",
-messagingSenderId: "709609702934"
+  apiKey: "AIzaSyCy06AYOySGIMILWjBfCYBpG-HIWNqcHQw",
+  authDomain: "retrieve-55769.firebaseapp.com",
+  databaseURL: "https://retrieve-55769.firebaseio.com",
+  projectId: "retrieve-55769",
+  storageBucket: "",
+  messagingSenderId: "569280491909"
 };
+
+// const config = {
+//   apiKey: "AIzaSyBrxoHAQNjn08MWXXMwyivst0nC2N2zjUg",
+//   authDomain: "retrieve-a82e2.firebaseapp.com",
+//   databaseURL: "https://retrieve-a82e2.firebaseio.com",
+//   projectId: "retrieve-a82e2",
+//   storageBucket: "retrieve-a82e2.appspot.com",
+//   messagingSenderId: "709609702934"
+// };
   
 export const firebaseApp = firebase.initializeApp(config);
 const getRef = () => firebaseApp.database().ref();
