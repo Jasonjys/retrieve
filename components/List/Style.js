fd8eb1e6cae1d19a82004e68e1ba68c672d4769b
Jasonjys <jasonjys0511@gmail.com>
2017-10-17 16:13:23 -0400

    latest

--- components/List/Style.js
+++ components/List/Style.js
@@ -3,10 +3,12 @@ import {StyleSheet} from 'react-native';
 export default StyleSheet.create({
   listContainer: {
     marginTop: 0,
-    borderTopColor: 'transparent'
+    borderTopColor: 'transparent',
+    width: '100%'
   },
   itemContainer: {
     flexDirection: 'row',
+    width: '100%',
     paddingLeft: 10,
     paddingTop: 5
   },
